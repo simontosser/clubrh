b034cdfb7e607f8cc7e41f857550954ea5571e60
Florian <florian@kornog-computing.com>
2018-02-06 16:59:42 +0100

    Get all the recrutementProfils for a specific user company or authority

--- src/main/java/com/poleemploi/cvtheque/service/RecrutementProfilService.java
+++ src/main/java/com/poleemploi/cvtheque/service/RecrutementProfilService.java
@@ -1,6 +1,7 @@
 package com.poleemploi.cvtheque.service;
 
 import com.poleemploi.cvtheque.service.dto.RecrutementProfilDTO;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -24,7 +25,23 @@ public interface RecrutementProfilService {
      * @return the list of entities
      */
     Page<RecrutementProfilDTO> findAll(Pageable pageable);
-
+    
+    /**
+     * Get all the recrutementProfils with current user company.
+     *
+     * @param pageable the pagination information
+     * @return the list of entities
+     */
+	Page<RecrutementProfilDTO> findAllWithCurrentUserCompany(Pageable pageable);
+	
+	/**
+     * Get all the recrutementProfils with not current user company.
+     *
+     * @param pageable the pagination information
+     * @return the list of entities
+     */
+	Page<RecrutementProfilDTO> findAllWithCurrentUserCompanyNot(Pageable pageable);
+	
     /**
      * Get the "id" recrutementProfil.
      *
--- src/main/java/com/poleemploi/cvtheque/service/impl/RecrutementProfilServiceImpl.java
+++ src/main/java/com/poleemploi/cvtheque/service/impl/RecrutementProfilServiceImpl.java
@@ -2,8 +2,13 @@ package com.poleemploi.cvtheque.service.impl;
 
 import com.poleemploi.cvtheque.service.RecrutementProfilService;
 import com.poleemploi.cvtheque.domain.RecrutementProfil;
+import com.poleemploi.cvtheque.domain.User;
+import com.poleemploi.cvtheque.repository.AuthorityRepository;
 import com.poleemploi.cvtheque.repository.RecrutementProfilRepository;
+import com.poleemploi.cvtheque.repository.UserRepository;
 import com.poleemploi.cvtheque.repository.search.RecrutementProfilSearchRepository;
+import com.poleemploi.cvtheque.security.AuthoritiesConstants;
+import com.poleemploi.cvtheque.security.SecurityUtils;
 import com.poleemploi.cvtheque.service.dto.RecrutementProfilDTO;
 import com.poleemploi.cvtheque.service.mapper.RecrutementProfilMapper;
 import org.slf4j.Logger;
@@ -30,11 +35,17 @@ public class RecrutementProfilServiceImpl implements RecrutementProfilService {
     private final RecrutementProfilMapper recrutementProfilMapper;
 
     private final RecrutementProfilSearchRepository recrutementProfilSearchRepository;
+    
+    private final UserRepository userRepository;
+    
+    private final AuthorityRepository authorityRepository;
 
-    public RecrutementProfilServiceImpl(RecrutementProfilRepository recrutementProfilRepository, RecrutementProfilMapper recrutementProfilMapper, RecrutementProfilSearchRepository recrutementProfilSearchRepository) {
+    public RecrutementProfilServiceImpl(RecrutementProfilRepository recrutementProfilRepository, RecrutementProfilMapper recrutementProfilMapper, RecrutementProfilSearchRepository recrutementProfilSearchRepository, UserRepository userRepository, AuthorityRepository authorityRepository) {
         this.recrutementProfilRepository = recrutementProfilRepository;
         this.recrutementProfilMapper = recrutementProfilMapper;
         this.recrutementProfilSearchRepository = recrutementProfilSearchRepository;
+        this.userRepository = userRepository;
+        this.authorityRepository = authorityRepository;
     }
 
     /**
@@ -54,7 +65,7 @@ public class RecrutementProfilServiceImpl implements RecrutementProfilService {
     }
 
     /**
-     * Get all the recrutementProfils.
+     * Get all the recrutementProfil.
      *
      * @param pageable the pagination information
      * @return the list of entities
@@ -62,10 +73,50 @@ public class RecrutementProfilServiceImpl implements RecrutementProfilService {
     @Override
     @Transactional(readOnly = true)
     public Page<RecrutementProfilDTO> findAll(Pageable pageable) {
-        log.debug("Request to get all RecrutementProfils");
-        
-        return recrutementProfilRepository.findAll(pageable)
-            .map(recrutementProfilMapper::toDto);
+        log.debug("Request to get all RecrutementProfil");
+        return SecurityUtils.getCurrentUserLogin()
+        .flatMap(userRepository::findOneByLogin)
+        .filter(u -> u.getAuthorities().contains(authorityRepository.findOne(AuthoritiesConstants.ADMIN)))
+        .map(t -> recrutementProfilRepository.findAll(pageable).map(recrutementProfilMapper::toDto))
+        .orElseGet(() -> this.findAllWithCurrentUserCompanyNot(pageable));
+    }
+
+    /**
+     * Get all the recrutementProfil for a specific user company.
+     *
+     * @param pageable the pagination information
+     * @return 
+     * @return the list of entities
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Page<RecrutementProfilDTO> findAllWithCurrentUserCompany(Pageable pageable) {
+        log.debug("Request to get all RecrutementProfil for a specific user company");
+        return SecurityUtils.getCurrentUserLogin()
+            .flatMap(userRepository::findOneByLogin)
+            .filter(a -> a.getCompany() != null)
+            .map(User::getCompany)
+            .map(company -> recrutementProfilRepository.findAllByCompany(pageable, company).map(recrutementProfilMapper::toDto))
+            .orElse(null);       
+    }
+    
+    /**
+     * Get all the RecrutementProfil for a specific user company.
+     *
+     * @param pageable the pagination information
+     * @return 
+     * @return the list of entities
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Page<RecrutementProfilDTO> findAllWithCurrentUserCompanyNot(Pageable pageable) {
+        log.debug("Request to get all RecrutementProfil for a specific user company");
+        return SecurityUtils.getCurrentUserLogin()
+            .flatMap(userRepository::findOneByLogin)
+            .filter(a -> a.getCompany() != null)
+            .map(User::getCompany)
+            .map(company -> recrutementProfilRepository.findAllByCompanyNot(pageable, company).map(recrutementProfilMapper::toDto))
+            .orElse(null);       
     }
 
     /**
